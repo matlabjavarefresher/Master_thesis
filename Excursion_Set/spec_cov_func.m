d2a373ddd0d950ab7106386fef1883d81934d176
matlabjavarefresher <premanart@gmail.com>
2016-10-24 13:51:01 +0200

    Revert "24_10"
    This reverts commit cb0ba8598fed319ff6bb8db0641bb180b07acd3c.

--- Excursion_Set/spec_cov_func.m
+++
@@ -1,4 +0,0 @@
-function c=spec_cov_func(d, A, alpha, omega, phi)
-
-    c=A*exp(-alpha*d).*sin(omega*d+phi);
-end
--- Voxel code/dist.m
+++
@@ -1,5 +0,0 @@
-function [ Dist ] = dist( x,y )
-[X,Y]=meshgrid(x,y);
-Dist=abs(X-Y);
-end
-
